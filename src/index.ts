31b234e919cc96de8222e44ed5452bf1f9be966e
Leon Adler <git@leon-adler.com>
2017-01-19 18:56:32 +0100

    Export classes added in 0.4.0

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
+import { CloneDepth } from './clone-depth-decorator';
 export { deepClone } from './deep-clone';
 export { deepEqual } from './deep-equal';
 export { flatEqual } from './flat-equal';
 export { Immutable } from './immutable-decorator';
 import { ChangeList } from './immutable-interfaces';
 export { immutableSettings } from './immutable-settings';
+export { ImmutableStateStore } from './immutable-state-store';
 export { isCyclicStructure } from './is-cyclic-structure';
 export { isImmutableClass } from './is-immutable-class';
 export { map } from './map';
 export { MethodNotImmutableError } from './method-not-immutable-error';
 export { observeImmutable } from './observe-immutable';
+export { StateActionBranch } from './state-action-branch';
 export { traverseObject } from './traverse-object';
 export { withChanges } from './with-changes';
